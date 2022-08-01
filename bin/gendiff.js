932a571ebe1f2be7b891d08f7761f233d4b8c4e9
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-08-01 09:23:34 +0300

    feat(executable): add program option and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,8 @@ import { program } from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0');
+  .version('0.1.0')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
