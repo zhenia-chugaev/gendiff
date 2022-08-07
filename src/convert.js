baf7f7e070b684064fdbbeffd19235f7a0a484e7
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-08-07 19:59:51 +0300

    feat: add convert function

---
+++ src/convert.js
@@ -0,0 +1,26 @@
+const SPACES_COUNT = 2;
+
+export default (keySpecs) => {
+  const inner = keySpecs
+    .flatMap((key) => {
+      const line = `${key.name}: ${key.initialValue}`;
+
+      switch (key.state) {
+        case 'added':
+          return `+ ${line}`;
+        case 'deleted':
+          return `- ${line}`;
+        case 'changed':
+          return [
+            `- ${line}`,
+            `+ ${key.name}: ${key.finalValue}`,
+          ];
+        default:
+          return `  ${line}`;
+      }
+    })
+    .map((line) => line.padStart(line.length + SPACES_COUNT, ' '))
+    .join('\n');
+
+  return `{\n${inner}\n}`;
+};
