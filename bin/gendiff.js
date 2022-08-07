ca73d5d68e9cf0c5c8845f00dca2c6b1f90bd16e
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-08-07 20:13:54 +0300

    feat(executable): add support for comparing .json

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import genDiff from '../src/gendiff.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0')
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>');
+
+program.action((arg1, arg2) => {
+  const output = genDiff(arg1, arg2);
+  console.log(output);
+});
 
 program.parse();
