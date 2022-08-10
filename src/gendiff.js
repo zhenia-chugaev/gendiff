94a9f26aec4e878db5d6d8996d08e903f8f6a110
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-08-10 11:12:12 +0300

    refactor: isolate side effects

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,11 +2,18 @@ import { readFileSync } from 'fs';
 import compare from './compare.js';
 import convert from './convert.js';
 
-export default (filepath1, filepath2) => {
-  const string1 = readFileSync(filepath1, 'utf8');
-  const string2 = readFileSync(filepath2, 'utf8');
+const process = (string1, string2) => {
   const object1 = JSON.parse(string1);
   const object2 = JSON.parse(string2);
   const diff = compare(object1, object2);
   return convert(diff);
 };
+
+export default (filepath1, filepath2) => {
+  const string1 = readFileSync(filepath1, 'utf8');
+  const string2 = readFileSync(filepath2, 'utf8');
+  const result = process(string1, string2);
+  return result;
+};
+
+export { process };
