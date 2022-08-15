b0b0f6e88f1e2638b94b36e70d2fe8d4056f9a0a
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-08-15 10:47:50 +0300

    test: add tests for flat yaml files

--- __tests__/src/gendiff.test.js
+++ __tests__/src/gendiff.test.js
@@ -5,6 +5,8 @@ import { process } from '../../src/gendiff.js';
 
 let json1;
 let json2;
+let yaml1;
+let yaml2;
 
 let result1;
 let result2;
@@ -13,6 +15,8 @@ let result3;
 beforeAll(() => {
   json1 = readFixture('file1.json');
   json2 = readFixture('file2.json');
+  yaml1 = readFixture('file1.yml');
+  yaml2 = readFixture('file2.yaml');
 
   result1 = readFixture('result1');
   result2 = readFixture('result2');
@@ -25,5 +29,14 @@ describe('json files testing', () => {
     expect(process(json2, json2)).toBe(result2);
     expect(process(json2, json1)).toBe(result3);
     expect(process('{}', '{}')).toBe('{}');
+  });
+});
+
+describe('yaml files testing', () => {
+  test('flat structures', () => {
+    expect(process(yaml1, yaml2)).toBe(result1);
+    expect(process(yaml2, yaml2)).toBe(result2);
+    expect(process(yaml2, yaml1)).toBe(result3);
+    expect(process('', '')).toBe('{}');
   });
 });
