09662fccd3850bc5a9f7658c0238f2cb1b24db83
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-08-15 10:52:46 +0300

    test: refactor existing tests

--- __tests__/src/gendiff.test.js
+++ __tests__/src/gendiff.test.js
@@ -3,25 +3,27 @@
 import readFixture from '../../src/fixture.js';
 import { process } from '../../src/gendiff.js';
 
-let string1;
-let string2;
+let json1;
+let json2;
 
 let result1;
 let result2;
 let result3;
 
 beforeAll(() => {
-  string1 = readFixture('file1.json');
-  string2 = readFixture('file2.json');
+  json1 = readFixture('file1.json');
+  json2 = readFixture('file2.json');
 
   result1 = readFixture('result1');
   result2 = readFixture('result2');
   result3 = readFixture('result3');
 });
 
-test('flat json files', () => {
-  expect(process(string1, string2)).toBe(result1);
-  expect(process(string2, string2)).toBe(result2);
-  expect(process(string2, string1)).toBe(result3);
-  expect(process('{}', '{}')).toBe('{}');
+describe('json files testing', () => {
+  test('flat structures', () => {
+    expect(process(json1, json2)).toBe(result1);
+    expect(process(json2, json2)).toBe(result2);
+    expect(process(json2, json1)).toBe(result3);
+    expect(process('{}', '{}')).toBe('{}');
+  });
 });
