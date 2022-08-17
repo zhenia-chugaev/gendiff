20913b6bd515a7b1cce874b008fb46454e599550
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-08-17 08:39:47 +0300

    feat: add support for comparing yaml files

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,18 +1,34 @@
+import { extname } from 'path';
 import { readFileSync } from 'fs';
+import YAML from 'yaml';
 import compare from './compare.js';
 import convert from './convert.js';
 
-const process = (string1, string2) => {
-  const object1 = JSON.parse(string1);
-  const object2 = JSON.parse(string2);
+const read = (filepath) => {
+  const extension = extname(filepath);
+  const content = readFileSync(filepath, 'utf8');
+  return { extension, content };
+};
+
+const parse = ({ content, extension }) => {
+  if (extension === '.yml' || extension === '.yaml') {
+    return YAML.parse(content);
+  }
+
+  return JSON.parse(content);
+};
+
+const process = (file1, file2) => {
+  const object1 = parse(file1);
+  const object2 = parse(file2);
   const diff = compare(object1, object2);
   return convert(diff);
 };
 
 export default (filepath1, filepath2) => {
-  const string1 = readFileSync(filepath1, 'utf8');
-  const string2 = readFileSync(filepath2, 'utf8');
-  const result = process(string1, string2);
+  const file1 = read(filepath1);
+  const file2 = read(filepath2);
+  const result = process(file1, file2);
   return result;
 };
 
