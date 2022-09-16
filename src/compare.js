6ef01e4df7c4c6801aff11ab5228b450295e5ef6
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-09-16 15:27:47 +0300

    refactor: make code immutable

--- src/compare.js
+++ src/compare.js
@@ -8,41 +8,42 @@ const makeSpecification = (key, value) => ({
   spec: null,
 });
 
+const assign = (object, attributes) => ({
+  ...object,
+  ...attributes,
+});
+
 const compare = (arg1, arg2) => {
   const object1 = !arg1 ? {} : arg1;
   const object2 = !arg2 ? {} : arg2;
-
-  const result = [];
-
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
 
-  keys1.forEach((key) => {
+  const specs1 = keys1.map((key) => {
     const value1 = object1[key];
     const value2 = object2[key];
     const spec = makeSpecification(key, value1);
-    result.push(spec);
 
     if (!keys2.includes(key)) {
-      spec.state = 'deleted';
-    } else if (_.isObject(value1) && _.isObject(value2)) {
-      spec.spec = compare(value1, value2);
-    } else if (value1 === value2) {
-      spec.state = 'unchanged';
-    } else {
-      spec.state = 'changed';
-      spec.finalValue = value2;
+      return assign(spec, { state: 'deleted' });
     }
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return assign(spec, { spec: compare(value1, value2) });
+    }
+    if (value1 === value2) {
+      return assign(spec, { state: 'unchanged' });
+    }
+
+    return assign(spec, { state: 'changed', finalValue: value2 });
   });
 
   const added = _.difference(keys2, keys1);
-
-  added.forEach((key) => {
+  const specs2 = added.map((key) => {
     const spec = makeSpecification(key, object2[key]);
-    spec.state = 'added';
-    result.push(spec);
+    return assign(spec, { state: 'added' });
   });
 
+  const result = specs1.concat(specs2);
   return _.sortBy(result, 'name');
 };
 
